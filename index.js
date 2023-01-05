12eec0fe650488e7ee56769fae2f03461ff2232b
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-05 16:31:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+let playerNumber;
+let deallerNumber;
+let hitMore;
+let playGame = confirm("Hello! Do you want to play some Black Jack today?");
+
+while (playGame) {
+  runTheGame();
+
+  // Ask if user wants to play again
+  playGame = confirm("Do you want to play again?");
+  if (!playGame) alert("Bye Bye, Looser!");
+}
+
+//RESET INITIAL SCORE IF ANY, AND RUN THE GAME
+function runTheGame() {
+  playerNumber = deal(21, 4);
+  deallerNumber = deal(11, 2);
+  hitMore = confirm(`You have ${playerNumber}, do you want to hit more?`);
+
+  dealPlayer();
+  dealDealler();
+  checkResults();
+}
+
+// Return random numer between min and max arguments
+function deal(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Deals player additional numer if player choose Hit more
+function dealPlayer() {
+  while (hitMore) {
+    playerNumber += deal(11, 2);
+
+    if (playerNumber >= 21) {
+      hitMore = false;
+    } else {
+      hitMore = confirm(`You have ${playerNumber}, do you want to hit more?`)
+    }
+  }
+}
+
+// Deals additional number for the dealler if dealler has less then 17
+function dealDealler() {
+  while (deallerNumber < 17) {
+    deallerNumber += deal(11, 2);
+  }
+}
+
+// CHECK THE FINAL RESULTS
+function checkResults() {
+  // If player has too many
+  if (playerNumber > 21) {
+    alert(`You have ${playerNumber}, That's too many..  DEALLER WINS!`);
+    return
+  }
+
+  // Iplayer has 21 or player number is bigger than dealler's number
+  if ((playerNumber === 21 && deallerNumber !== 21)
+    || (playerNumber < 21 && playerNumber > deallerNumber)) {
+    alert(`Your score is ${playerNumber} and Dealler has ${deallerNumber}. ==== YOU WIN ====!`);
+    return
+  }
+  // If draw
+  if (playerNumber === deallerNumber) { 
+    alert(`Your score is ${playerNumber}, and Dealler has ${deallerNumber}.  That's the DRAW!`);
+    return
+  }
+
+  // Idealler has too many
+  if (deallerNumber > 21) {
+    alert(`Dealler has ${deallerNumber}, that's too many.  ==== YOU WIN ====!`);
+    return
+  }
+
+  // Other not winning scenarios
+  alert(`Your score is ${playerNumber} and Dealler has ${deallerNumber}.  DEALLER WINS!`);
+}
+
